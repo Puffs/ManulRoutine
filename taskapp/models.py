13f71891f6c33d2c1c19621ffb72e16064bfb7c0
Puffs <andrey.antonov1999@mail.ru>
2025-07-26 10:29:25 +0500

    user field to task

--- taskapp/models.py
+++ taskapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from boardapp.models import Column
+from userapp.models import CustomUser
 from datetime import datetime
 
 class Task(models.Model):
@@ -7,8 +8,11 @@ class Task(models.Model):
     typ = models.CharField(max_length=10, blank=True, null=True)
     order = models.IntegerField(blank=True, null=True)
     column = models.ForeignKey(Column, on_delete=models.CASCADE)
+    # executor = models.ForeignKey(CustomUser, on_delete=models.CASCADE, blank=True, null=True)
+    executor = models.ManyToManyField(CustomUser, related_name='tasks')
 
 class Comment(models.Model):
     text = models.TextField()
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     date = models.DateTimeField(default=datetime.now())
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
