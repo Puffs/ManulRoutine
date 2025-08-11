1fe42bed5fbc4c5ff8808cd3f92e47680a327ae4
Puffs <andrey.antonov1999@mail.ru>
2025-08-11 16:00:48 +0500

    date format and executors serializer

--- taskapp/serializers.py
+++ taskapp/serializers.py
@@ -4,14 +4,25 @@ from rest_framework import serializers
 from rest_framework.response import Response
 from django.db import IntegrityError
 from rest_framework import status
+from userapp.models import CustomUser
+from userapp.serializers import CustomUserSelectiveSerializer
 
-
+    
 class CommentInlineSerializer(ModelSerializerId):
+    date= serializers.DateTimeField(
+        format='%d.%m.%Y %H:%M',
+        read_only=True,
+    )
+    author = CustomUserSelectiveSerializer(read_only=True)
     class Meta:
         model = Comment
         fields = "__all__"
 
 class CommentSerializer(serializers.ModelSerializer):
+    date= serializers.DateTimeField(
+        format='%d.%m.%Y %H:%M',
+        read_only=True,
+    )
     class Meta:
         model = Comment
         fields = "__all__"
@@ -19,12 +30,14 @@ class CommentSerializer(serializers.ModelSerializer):
         
 class TaskInlineSerializer(ModelSerializerId):
     comment_set = CommentInlineSerializer(many=True, read_only=True)
+    executor = CustomUserSelectiveSerializer(many=True, read_only=True)
     class Meta:
         model = Task
         fields = "__all__"
 
 class TaskSerializer(serializers.ModelSerializer):
     comment_set = CommentInlineSerializer(many=True, read_only=True)
+    executor = CustomUserSelectiveSerializer(many=True, read_only=True)
     class Meta:
         model = Task
         fields = "__all__"
