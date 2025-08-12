0196f0fae9f4119011e1989eec1f0f5a412b9cff
Puffs <andrey.antonov1999@mail.ru>
2025-08-12 08:44:20 +0500

    fix user serializer

--- taskapp/serializers.py
+++ taskapp/serializers.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from django.db import IntegrityError
 from rest_framework import status
 from userapp.models import CustomUser
-from userapp.serializers import CustomUserSelectiveSerializer
+from userapp.serializers import CustomUserSerializer
 
     
 class CommentInlineSerializer(ModelSerializerId):
@@ -13,7 +13,7 @@ class CommentInlineSerializer(ModelSerializerId):
         format='%d.%m.%Y %H:%M',
         read_only=True,
     )
-    author = CustomUserSelectiveSerializer(read_only=True)
+    author = CustomUserSerializer(read_only=True)
     class Meta:
         model = Comment
         fields = "__all__"
@@ -30,15 +30,26 @@ class CommentSerializer(serializers.ModelSerializer):
         
 class TaskInlineSerializer(ModelSerializerId):
     comment_set = CommentInlineSerializer(many=True, read_only=True)
-    executor = CustomUserSelectiveSerializer(many=True, read_only=True)
+    executor = CustomUserSerializer(many=True, read_only=True)
     class Meta:
         model = Task
         fields = "__all__"
 
 class TaskSerializer(serializers.ModelSerializer):
     comment_set = CommentInlineSerializer(many=True, read_only=True)
-    executor = CustomUserSelectiveSerializer(many=True, read_only=True)
+    executor = serializers.PrimaryKeyRelatedField(queryset=CustomUser.objects.all(),many=True)
     class Meta:
         model = Task
         fields = "__all__"
 
+    def update(self,instance,validated_data):
+        executor_set = validated_data.pop('executor', None)
+        
+        if executor_set is not None:
+            instance.executor.set(executor_set)
+        return super().update(instance,validated_data)
+    
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['executor'] = CustomUserSerializer(instance.executor.all(), many=True).data
+        return representation
--- userapp/serializers.py
+++ userapp/serializers.py
@@ -5,20 +5,24 @@ from rest_framework.response import Response
 from django.db import IntegrityError
 from rest_framework import status
 
-class CustomUserSelectiveSerializer(serializers.ModelSerializer):
-    avatar_url = serializers.SerializerMethodField()
-    class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'avatar_url']
-    def get_avatar_url(self, obj):
-        return obj.avatar.url if obj.avatar else None
+# class CustomUserSelectiveSerializer(serializers.ModelSerializer):
+#     avatar_url = serializers.SerializerMethodField()
+#     class Meta:
+#         model = CustomUser
+#         fields = ['id', 'username', 'avatar_url']
+#     def get_avatar_url(self, obj):
+#         return obj.avatar.url if obj.avatar else None
     
 class CustomUserInlineSerializer(ModelSerializerId):
     class Meta:
         model = CustomUser
-        fields = "__all__"
+        exclude = ['password']
 
 class CustomUserSerializer(serializers.ModelSerializer):
+    avatar_url = serializers.SerializerMethodField()
     class Meta:
         model = CustomUser
-        fields = "__all__"
+        exclude = ['password']
+    
+    def get_avatar_url(self, obj):
+        return obj.avatar.url if obj.avatar else None
